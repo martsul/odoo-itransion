28f54e2d56b73e0b3812806812400bdeb7faf611
martsul <sasham9321@mail.ru>
2025-05-10 16:26:33 +0300

    m

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM odoo:17
 
 COPY ./odoo.conf /etc/odoo/odoo.conf
 COPY ./addons /mnt/extra-addons
-
 COPY ./entrypoint.sh /entrypoint.sh
+
 RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-odoo -c /etc/odoo/odoo.conf -i base --without-demo=all
+ODOO_DB_NAME=${DB_NAME:-odoo_db}
+ODOO_DB_USER=${DB_USER:-odoo_user}
+ODOO_DB_PASSWORD=${DB_PASSWORD:-odoo_pass}
+ODOO_DB_HOST=${DB_HOST:-postgres}
+ODOO_DB_PORT=${DB_PORT:-5432}
 
-exec odoo -c /etc/odoo/odoo.conf
+TABLE_COUNT=$(psql -h "$ODOO_DB_HOST" -U "$ODOO_DB_USER" -d "$ODOO_DB_NAME" -tAc "SELECT count(*) FROM information_schema.tables WHERE table_schema='public';")
+
+if [ "$TABLE_COUNT" -eq "0" ]; then
+    echo ">> Initial DB..."
+    exec odoo -i base --config=/etc/odoo/odoo.conf
+else
+    echo ">> DB Was Founded..."
+    exec odoo --config=/etc/odoo/odoo.conf
+fi
